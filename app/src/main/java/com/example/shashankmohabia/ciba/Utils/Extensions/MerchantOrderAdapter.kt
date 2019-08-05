cceb948043b18c3e76741b4d7c7fe83b598322d6
killbotxd <sonawane.1@iitj.ac.in>
2019-08-05 17:02:37 +0530

    change merchant orders adapter to show child rv on click

--- app/src/main/java/com/example/shashankmohabia/ciba/Utils/Extensions/MerchantOrderAdapter.kt
+++ app/src/main/java/com/example/shashankmohabia/ciba/Utils/Extensions/MerchantOrderAdapter.kt
@@ -1,6 +1,8 @@
 package com.example.shashankmohabia.ciba.Utils.Extensions
 
 import android.content.Context
+import android.opengl.Visibility
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -8,23 +10,59 @@ import android.view.ViewGroup
 import android.widget.RelativeLayout
 import android.widget.TextView
 import android.widget.Toast
+import com.example.shashankmohabia.ciba.Core.db
 import com.example.shashankmohabia.ciba.R
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.firestore.Query
+import kotlinx.android.synthetic.main.activity_cart.view.*
+import kotlinx.android.synthetic.main.merchant_item.view.*
 
 class MerchantOrderAdapter(options: FirestoreRecyclerOptions<OrderData>, private val mContext: Context?) : FirestoreRecyclerAdapter<OrderData, MerchantOrderAdapter.ItemHolderMerchant>(options) {
+var adapterMerchantOrderChild:OrderChildAdapter ? =null
 
 
     override fun onBindViewHolder(holder: ItemHolderMerchant, position: Int, model: OrderData) {
-
+        var open:Boolean=true
+        val query:Query=db.collection("Orders1/${model.orderId}$/items")
         holder.placedBy.text=model.placedBy
         holder.placedByNumber.text=model.placedByNumber
         holder.placedByTime.text=model.time.toString()
+        holder.cardLayout.setOnClickListener {
+            if(!open){
+             setupChildRV(query,holder)
+                adapterMerchantOrderChild!!.startListening()
+            holder.itemView.child_rv_card.visibility=View.VISIBLE
+            holder.itemView.child_rv.visibility=View.VISIBLE
+            holder.itemView.total_qty_text.visibility=View.VISIBLE
+            holder.itemView.Order_qty.visibility=View.VISIBLE
+            holder.itemView.total_amt_text.visibility=View.VISIBLE
+            holder.itemView.total_amt.visibility=View.VISIBLE}
+            else if(open){
+                adapterMerchantOrderChild!!.stopListening()
+                holder.itemView.child_rv_card.visibility=View.INVISIBLE
+                holder.itemView.child_rv.visibility=View.INVISIBLE
+                holder.itemView.total_qty_text.visibility=View.INVISIBLE
+                holder.itemView.Order_qty.visibility=View.INVISIBLE
+                holder.itemView.total_amt_text.visibility=View.INVISIBLE
+                holder.itemView.total_amt.visibility=View.INVISIBLE
 
+            }
+        }
 
-        //add onclick listener?
     }
 
+    private fun setupChildRV(query: Query,holder: ItemHolderMerchant) {
+        val options: FirestoreRecyclerOptions<OrderChildModel> = FirestoreRecyclerOptions.Builder<OrderChildModel>()
+                .setQuery(query, OrderChildModel::class.java)
+                .build()
+        adapterMerchantOrderChild=OrderChildAdapter(options)
+        holder.itemView.child_rv.layoutManager=LinearLayoutManager(this.mContext)
+        holder.itemView.child_rv.adapter=adapterMerchantOrderChild
+
+
+
+    }
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolderMerchant {
